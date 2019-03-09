5a094838efe714a00b46f0fcc8be71cf2c6dd0d4
Pravin <work.coderpravin@gmail.com>
2019-03-09 16:30:32 +0530

    Removed eslint from directory.

--- src/pages/RegistrationPage/index.js
+++ src/pages/RegistrationPage/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import RegistrationForm from '../../component/RegistrationForm';
+import React from "react";
+import PropTypes from "prop-types";
+import RegistrationForm from "../../component/RegistrationForm";
 
 const RegistrationPage = props => {
-  const { match, history } = props;
-  const { path } = match || {};
+  const {match, history} = props;
+  const {path} = match || {};
   return (
     <div>
-      <RegistrationForm path={path} history={history} />
+      <RegistrationForm path={path} history={history}/>
     </div>
   );
 };
 
 RegistrationPage.propTypes = {
   match: PropTypes.shape({}).isRequired,
-  history: PropTypes.shape({}).isRequired,
+  history: PropTypes.shape({}).isRequired
 };
 
 export default RegistrationPage;
